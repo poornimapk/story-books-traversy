d587300ae64672e556f6f1ae781ad567328dbbfd
poornimapk <poornima.pk@gmail.com>
2022-08-05 16:01:55 -0700

    Added routes and views, setup the Google oauth in env

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const path = require('path');
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const passport = require("passport");
+const session = require('express-session')
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -19,6 +25,23 @@ if(process.env.NODE_ENV === "development"){
 // Handlebars
 app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
+
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+  }))
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/', require('./routes/index'));
 
 const PORT = process.env.PORT || 3030;
 
