9320caf0d031b0f11887ab8f5aaaf34adc04d67e
poornimapk <poornima.pk@gmail.com>
2022-08-07 18:04:52 -0700

    Added google authentication

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const express = require("express");
 const dotenv = require("dotenv");
-const connectDB = require("./config/db");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
 const passport = require("passport");
-const session = require('express-session')
+const session = require('express-session');
+const connectDB = require("./config/db");
 
 // Load config
 dotenv.config({ path: './config/config.env' });
@@ -23,7 +23,10 @@ if(process.env.NODE_ENV === "development"){
 }
 
 // Handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}));
+app.engine('.hbs', exphbs.engine({ 
+    defaultLayout: 'main', 
+    extname: '.hbs'
+}));
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -42,6 +45,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3030;
 
