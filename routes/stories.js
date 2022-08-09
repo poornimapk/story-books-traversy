51f8b12b12bc3dd58b7e2f945153d257ef9c2e82
poornimapk <poornima.pk@gmail.com>
2022-08-09 13:47:21 -0700

    Added edit, delete in dashboard

--- routes/stories.js
+++ routes/stories.js
@@ -42,4 +42,65 @@ router.get('/', ensureAuth, async (req, res) => {
     }    
 });
 
+// @desc Show edit page
+// @route GET /stories/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+    try{
+        const story = await Story.findOne({
+            _id: req.params.id
+        }).lean();
+        if(!story) {
+            return res.render('error/404');
+        }
+        if(story.user != req.user.id) {
+            res.redirect('/stories');
+        } else {
+            res.render('stories/edit', {
+                story,
+            })
+        }
+    } catch (err){
+        console.error(err);
+        return res.render('error/500');
+    }    
+});
+
+// @desc Update story
+// @route PUT /stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+    try{
+        let story = await Story.findById(req.params.id).lean();
+        if(!story) {
+            return res.render('error/404');
+        }
+        if(story.user != req.user.id) {
+            res.redirect('/stories');
+        } else {
+            story = await Story.findOneAndUpdate({
+                _id: req.params.id
+            }, req.body, {
+                new: true,
+                runValidators: true
+            });
+
+            res.redirect('/dashboard');
+        }
+    } catch (err){
+        console.error(err);
+        return res.render('error/500');
+    }
+});
+
+// @desc Delete story
+// @route DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+    try{
+        await Story.remove({_id: req.params.id});
+        res.redirect('/dashboard');
+    }catch (err){
+        console.error(err);
+        return res.render('error/500');
+    }
+});
+
 module.exports = router;
