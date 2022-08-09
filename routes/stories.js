0e8c37769679aa6a0318171b83c0c0ed3f1ab6cc
poornimapk <poornima.pk@gmail.com>
2022-08-09 14:21:18 -0700

    Added single user stories functionality

--- routes/stories.js
+++ routes/stories.js
@@ -42,6 +42,26 @@ router.get('/', ensureAuth, async (req, res) => {
     }    
 });
 
+// @desc Show Single story
+// @route GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+    try{
+        let story = await Story.findById(req.params.id)
+                .populate('user')
+                .lean();
+        if(!story) {
+            return res.render('error/404');
+        }
+
+        res.render('stories/show', {
+            story
+        });
+    } catch(err) {
+        console.error(err);
+        res.render('error/404');
+    }
+});
+
 // @desc Show edit page
 // @route GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -100,6 +120,26 @@ router.delete('/:id', ensureAuth, async (req, res) => {
     }catch (err){
         console.error(err);
         return res.render('error/500');
+    }
+});
+
+// @desc User stories
+// @route GET /stories/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+    try{
+        const stories = await Story.find({
+            user: req.params.userId,
+            status: 'public'
+        })
+        .populate('user')
+        .lean();
+
+        res.render('stories/index', {
+            stories
+        })
+    } catch(err) {
+        console.error(err);
+        res.render('error/500');
     }
 });
 
